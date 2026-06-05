3ff8b0567c8a107554e966c0f0281e1205e2d507
Skripa4illo <ivan.skriptunov@yandex.ru>
2026-06-05 13:51:51 +0800

    looking for the blocks2

--- includes/Fromtend/Class-Form-Block.php
+++ includes/Fromtend/Class-Form-Block.php
@@ -4,22 +4,16 @@ namespace AFB\Frontend;
 class Class_Form_Block {
 
 	public function __construct() {
+		// Само объявление типа блока оставляем на init
 		add_action( 'init', [ $this, 'register_gutenberg_block' ] );
+
+		// А подключение JS-скрипта переносим на специальный хук редактора!
+		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_script' ] );
 	}
 
 	public function register_gutenberg_block() {
-		// 1. Регистрируем JS-скрипт для редактора
-		wp_register_script(
-			'afb-form-block-editor',
-			AFB_URL . 'assets/js/block-form.js',
-			[ 'wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components' ], // Зависимости WP
-			AFB_VERSION,
-			true
-		);
-
-		// 2. Регистрируем сам блок и связываем его со скриптом
+		// Регистрируем ТОЛЬКО блок. Связь по имени скрипта в FSE-темах часто отваливается на ранних хуках.
 		register_block_type( 'afb/form-block', [
-			'editor_script'   => 'afb-form-block-editor', // Говорим WP загрузить наш скрипт в админку
 			'render_callback' => [ $this, 'render_block_html' ],
 			'attributes'      => [
 				'formId' => [
@@ -30,13 +24,23 @@ class Class_Form_Block {
 		] );
 	}
 
+	public function enqueue_block_editor_script() {
+		// Этот хук гарантирует, что 'wp-blocks', 'wp-element' и др. уже существуют в системе!
+		wp_enqueue_script(
+			'afb-form-block-editor',
+			AFB_URL . 'assets/js/block-form.js',
+			[ 'wp-blocks', 'wp-element', 'wp-block-editor', 'wp-components' ],
+			AFB_VERSION,
+			true
+		);
+	}
+
 	/**
 	 * Колбэк для рендеринга блока на фронтенде
 	 */
 	public function render_block_html( $attributes ) {
 		$form_id = isset( $attributes['formId'] ) ? absint( $attributes['formId'] ) : 1;
 
-		// Вызываем наш стандартный рендерер, который мы проверили
 		$renderer = new Class_Form_Render();
 		return $renderer->render_form_shortcode( [ 'id' => $form_id ] );
 	}
