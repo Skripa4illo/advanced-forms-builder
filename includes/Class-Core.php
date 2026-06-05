408ef53d9d765d4b09013a9cc24149537fe0e850
Skripa4illo <ivan.skriptunov@yandex.ru>
2026-06-05 12:38:11 +0800

    Fix REST URL punycode domain issue and clean core load

--- includes/Class-Core.php
+++ includes/Class-Core.php
@@ -5,7 +5,6 @@ class Class_Core {
 
 	public function run() {
 		$this->load_dependencies();
-		$this->init_hooks();
 	}
 
 	private function load_dependencies() {
@@ -13,20 +12,10 @@ class Class_Core {
 			new Admin\Class_Admin_Menu();
 		}
 
-		// Загружаем фронтенд-рендер и обработчик формы только на самом фронтенде
-		// или если идет запрос именно к нашему роуту API
-		if ( ! is_admin() ) {
-			new Frontend\Class_Form_Render();
-			new Frontend\Class_Form_Handler();
-		}
-	}
+		// Запускаем рендеринг шорткодов
+		new Frontend\Class_Form_Render();
 
-	private function init_hooks() {
-		// Общие хуки (например, локализация или API)
-		add_action( 'init', [ $this, 'init_plugin' ] );
-	}
-
-	public function init_plugin() {
-		load_plugin_textdomain( 'advanced-forms-builder', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+		// Инициализируем обработчик API стандартным для WP способом
+		new Frontend\Class_Form_Handler();
 	}
 }
