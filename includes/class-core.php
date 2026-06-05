96c62b1fc0585b7e3f9fde7ab8182adff568f15a
Skripa4illo <ivan.skriptunov@yandex.ru>
2026-06-05 21:17:08 +0800

    db update2

--- includes/class-core.php
+++ includes/class-core.php
@@ -5,10 +5,12 @@ class Class_Core {
 
     public function run() {
         $this->load_dependencies();
+        
+        // Регистрируем фронтенд скрипты через правильный хук WP
+        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_frontend_assets' ] );
     }
 
     private function load_dependencies() {
-        // Жестко подключаем файлы (пока автозагрузчик отдыхает на подстраховке)
         require_once AFB_PATH . 'includes/frontend/class-form-render.php';
         require_once AFB_PATH . 'includes/frontend/class-form-handler.php';
         require_once AFB_PATH . 'includes/frontend/class-form-block.php';
@@ -17,9 +19,21 @@ class Class_Core {
             new Admin\Class_Admin_Menu();
         }
 
-        // Инициализируем компоненты АБСОЛЮТНО жестко от корня неймспейса
         new \AFB\frontend\Class_Form_Render();
         new \AFB\frontend\Class_Form_Handler();
         new \AFB\frontend\Class_Form_Block();
+    }
+
+    /**
+     * Подключаем JS-скрипты для фронтенда сайта
+     */
+    public function enqueue_frontend_assets() {
+        wp_enqueue_script(
+            'afb-frontend-form-script',
+            AFB_URL . 'assets/js/frontend-form.js',
+            [],
+            time(), // Временно time() для сброса кэша Hostinger при деплое
+            true   // Жестко грузим в подвале (footer), чтобы DOM успел собраться
+        );
     }
 }
